2c80832e4be836e64501b909700311845d8dc7ca
aissaali2009-design <aissaali2009@gmail.com>
2026-09-05 23:19:21 +0100

    Change bot activity name from 'Custom Status' to 'Stalking'

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,7 +23,7 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Custom Status", // required by Discord API, not shown in the client
+        name: "Stalking", // required by Discord API, not shown in the client
         state: "Nexus",     // this is what people actually see
         type: 1,               // Custom
       },
