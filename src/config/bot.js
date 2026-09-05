e5f49ba7edd6f17444f6ec4a93d8481c800f4602
aissaali2009-design <aissaali2009@gmail.com>
2026-09-05 23:55:56 +0100

    Update bot activity state to 'Work & Progress'

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Watching", // required by Discord API, not shown in the client
-        state: "Nexus Ftw",     // this is what people actually see
+        state: "Work & Progress",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
