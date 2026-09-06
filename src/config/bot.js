7a86d16207b91efbce60e4e2760548ccbf926e42
aissaali2009-design <aissaali2009@gmail.com>
2026-09-06 12:54:51 +0100

    Update bot activity name and type

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,9 +23,9 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Watching", // required by Discord API, not shown in the client
+        name: "Be !!", // required by Discord API, not shown in the client
         state: "Work & Progress",     // this is what people actually see
-        type: 4,               // Custom
+        type: 5,               // Custom
       },
     ],
   },
